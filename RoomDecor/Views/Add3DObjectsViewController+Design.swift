6808cb1ec803834f50c87b96407d439450790feb
Antonio Markotic <antoniomarkotic1@gmail.com>
2024-01-06 21:52:37 +0100

    Two placeholder VCs and navigation

---
+++ RoomDecor/Views/Add3DObjectsViewController+Design.swift
@@ -0,0 +1,20 @@
+import UIKit
+import SnapKit
+
+extension Add3DObjectsViewController {
+
+    func buildViews() {
+        createViews()
+        styleViews()
+        defineLayoutForViews()
+    }
+
+    public func createViews() { }
+
+    public func styleViews() {
+        view.backgroundColor = .green
+    }
+
+    public func defineLayoutForViews() { }
+
+}
