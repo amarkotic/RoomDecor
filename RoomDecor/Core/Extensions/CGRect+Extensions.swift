b9d19214c2ccc84780d8dab809a4c596cfc521bb
Antonio Markotic <antoniomarkotic1@gmail.com>
2024-01-07 18:25:27 +0100

    Implemented screen with mocked virtual object

---
+++ RoomDecor/Core/Extensions/CGRect+Extensions.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension CGRect {
+
+    var mid: CGPoint {
+        return CGPoint(x: midX, y: midY)
+    }
+
+}
--- RoomDecor/Views/Add3DObjects/Add3DObjectsViewController+Design.swift
+++ RoomDecor/Views/Add3DObjects/Add3DObjectsViewController+Design.swift
@@ -1,3 +1,4 @@
+import ARKit
 import UIKit
 import SnapKit
 
@@ -9,12 +10,30 @@ extension Add3DObjectsViewController {
         defineLayoutForViews()
     }
 
-    public func createViews() { }
+    public func createViews() {
+        sceneView = ARSCNView()
+        sceneView.setUp(session)
+        view.addSubview(sceneView)
 
-    public func styleViews() {
-        view.backgroundColor = .green
+        addVirtualObjectButton = UIButton()
+        view.addSubview(addVirtualObjectButton)
     }
 
-    public func defineLayoutForViews() { }
+    public func styleViews() {
+        addVirtualObjectButton.setTitle("Add Virtual Object", for: .normal)
+        addVirtualObjectButton.backgroundColor = .systemBlue
+    }
+
+    public func defineLayoutForViews() {
+        sceneView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        addVirtualObjectButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(20)
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(buttonSize)
+        }
+    }
 
 }
