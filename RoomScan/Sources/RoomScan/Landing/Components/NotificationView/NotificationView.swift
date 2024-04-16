b2f6a2d194da4df0378bc899684b706805a3e7e1
Antonio Markotic <antoniomarkotic1@gmail.com>
2024-04-16 19:24:07 +0200

    Add notification for lidar

---
+++ RoomScan/Sources/RoomScan/Landing/Components/NotificationView/NotificationView.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class NotificationView: UIView {
+
+    let defaultPadding: CGFloat = 8
+    let cornerRadius: CGFloat = .doublePadding
+
+    var stackView: UIStackView!
+    var labelOne: UILabel!
+    var labelTwo: UILabel!
+    var labelThree: UILabel!
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        buildViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
